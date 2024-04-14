928f3edbd27b97dd095b8685bd44f600561931a0
sgupge2306 <21238029@edu.cc.saga-u.ac.jp>
2024-04-14 11:33:47 +0900

    保存できるようにした

--- front/src/Pages/PostPage.js
+++ front/src/Pages/PostPage.js
@@ -1,84 +1,65 @@
-import React, { useState, useEffect } from 'react'
+import * as React from 'react'
 import axios from 'axios';
-import { useParams } from 'react-router-dom';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import { useNavigate } from 'react-router-dom';
 
-const baseURL = "http://localhost:8080/post/"
+const baseURL = "http://127.0.0.1:8080/post/"
 
-function Postpage(){
-    const[posts, setPosts] = useState()
-    const {user_id } = useParams();
-    const[title, setTitle] = useState('');
-    const[hostname, setHostname] = useState('');
-    const[deadline, setDeadline] = useState('');
-    const[date_and_location, setDate_and_location] = useState('');
-    const[num_of_people, setNum_of_people] = useState('');
-    const[estimated_time, setEstimated_time] = useState('');
-    const[target, setTarget] = useState('');
-    const[tag, setTag] = useState('');
-    const[body, setBody] = useState('');
-    const[data_added, setData_added] = useState('');
+const Postpage=() => {
+    const[title, setTitle] = React.useState('a');
+    const[hostname, setHostname] = React.useState('a');
+    const[deadline, setDeadline] = React.useState('2024-03-23T03:40:01.891007Z');
+    const[date_and_location, setDate_and_location] = React.useState('a');
+    const[num_of_people, setNum_of_people] = React.useState('7');
+    const[estimated_time, setEstimated_time] = React.useState('7');
+    const[target, setTarget] = React.useState('a');
+    const[tag, setTag] = React.useState('a');
+    const[body, setBody] = React.useState('a');
+    const[data_added, setData_added] = React.useState('2024-03-22');
     const Post = async () => {
-        const titleOk = title.length != 0
-        const hostnameOk = hostname.length != 0
-        const deadlineOk = deadline.length != 0
-        const date_and_locationOk = date_and_location.length != 0
-        const num_of_peopleOk = num_of_people.length != 0
-        const estimated_timeOk =  estimated_time.length != 0
-        const targetOk = target != 0
-        const tagOk = tag.length != 0
-        const bodyOk = body.length != 0
-        const data_addedOk = data_added.length != 0
+        // console.log('post')
+        const titleOk = title.length !== 0;
+        const hostnameOk = hostname.length !== 0;
+        const deadlineOk = deadline.length !== 0;
+        const date_and_locationOk = date_and_location.length !== 0;
+        const num_of_peopleOk = num_of_people.length !== 0;
+        const estimated_timeOk =  estimated_time.length !== 0;
+        const targetOk = target !== 0;
+        const tagOk = tag.length !== 0;
+        const bodyOk = body.length !== 0;
+        const data_addedOk = data_added.length !== 0;
         if(titleOk && hostnameOk && deadlineOk && date_and_locationOk && num_of_peopleOk && estimated_timeOk && targetOk && tagOk && bodyOk && data_addedOk) {
             await axios.post(baseURL,{
                 title: String(title),
                 hostname: String(hostname),
                 deadline: String(deadline),
-                date_and_location: Text(date_and_location),
-                num_of_people: String(num_of_people),
-                estimated_time: String(estimated_time),
-                target: Text(target),
-                tag: Text(tag),
-                body: Text(body),
-                data_added: Date(data_added)
+                date_and_location: String(date_and_location),
+                num_of_people: Number(num_of_people),
+                estimated_time: Number(estimated_time),
+                target: String(target),
+                tag: String(tag),
+                body: String(body),
+                data_added: String(data_added)
             })
             .then(() => {
                 setTitle('');
-                setHostname('')
-                setDeadline('')
-                setDate_and_location('')
-                setNum_of_people('')
-                setEstimated_time('')
-                setTarget('')
-                setTag('')
-                setBody('')
-                setData_added('')
+                setHostname('');
+                setDeadline('');
+                setDate_and_location('');
+                setNum_of_people('');
+                setEstimated_time('');
+                setTarget('');
+                setTag('');
+                setBody('');
+                setData_added('');
             })
         }
     }
+    // const test = () => {console.log('test')}
     
-    // useEffect(() => {
-    //     axios.get()
-    //     .then(response => {
-    //         setPosts(response.data);
-    //         // console.log(response.data)
-    //     })
-    //     .catch(error => {
-    //         console.error('error',error);
-    //     });
-    // }, []);
-
     
-
-
-
-
-    
-
     return (
         <>
         <Box>
@@ -86,6 +67,7 @@ function Postpage(){
                 <TextField
                     id= "outlined-multiline-flexible"
                     label = "タイトル"
+                    value={title}
                     onChange={(e)=>{setTitle(e.target.value)}}
                 />
             </Grid>
@@ -93,6 +75,7 @@ function Postpage(){
                 <TextField
                     id= "outlined-multiline-flexible"
                     label = "投稿する人の名前"
+                    value={hostname}
                     onChange={(e)=>{setHostname(e.target.value)}}
                 />
                 
@@ -101,6 +84,7 @@ function Postpage(){
                 <TextField
                     id= "outlined-multiline-flexible"
                     label = "募集期間"
+                    value={deadline}
                     onChange={(e)=>{setDeadline(e.target.value)}}
                 />
             </Grid>
@@ -108,6 +92,7 @@ function Postpage(){
                 <TextField
                     id= "outlined-multiline-flexible"
                     label = "日時・場所"
+                    value={date_and_location}
                     onChange={(e)=>{setDate_and_location(e.target.value)}}
                 />
             </Grid>
@@ -115,6 +100,7 @@ function Postpage(){
                 <TextField
                     id= "outlined-multiline-flexible"
                     label = "人数"
+                    value={num_of_people}
                     onChange={(e)=>{setNum_of_people(e.target.value)}}
                 />
             </Grid>
@@ -122,6 +108,7 @@ function Postpage(){
                 <TextField
                     id= "outlined-multiline-flexible"
                     label = "見込み労働時間"
+                    value={estimated_time}
                     onChange={(e)=>{setEstimated_time(e.target.value)}}
                 />
             </Grid>
@@ -129,6 +116,7 @@ function Postpage(){
                 <TextField
                     id= "outlined-multiline-flexible"
                     label = "対象者"
+                    value={target}
                     onChange={(e)=>{setTarget(e.target.value)}}
                 />
             </Grid>
@@ -136,6 +124,7 @@ function Postpage(){
                 <TextField
                     id= "outlined-multiline-flexible"
                     label = "タグ"
+                    value={tag}
                     onChange={(e)=>{setTag(e.target.value)}}
                 />
             </Grid>
@@ -143,6 +132,7 @@ function Postpage(){
                 <TextField
                     id= "outlined-multiline-flexible"
                     label = "内容"
+                    value={body}
                     onChange={(e)=>{setBody(e.target.value)}}
                 />
             </Grid>
